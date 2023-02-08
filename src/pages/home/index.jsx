4d0554050259e8d385cd7b3f26b09f7aeb76e7d7
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2023-02-08 19:57:34 -0300

    update home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,24 @@
-import './css/main.css'
+import './css/main.css';
 
 import { useState } from 'react';
 
-import { ReactComponent as LightDefault } from './assets/light-default.svg'
-import { ReactComponent as LightHover } from './assets/light-hover.svg'
-import { ReactComponent as DarkDefault } from './assets/dark-default.svg'
-import { ReactComponent as DarkHover } from './assets/dark-hover.svg'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
-import challenge1 from '../challenge1/assets/desktop.png'
+import { ReactComponent as LightDefault } from './assets/light-default.svg';
+import { ReactComponent as LightHover } from './assets/light-hover.svg';
+import { ReactComponent as DarkDefault } from './assets/dark-default.svg';
+import { ReactComponent as DarkHover } from './assets/dark-hover.svg';
+import { ReactComponent as Github } from './assets/github.svg';
+import { ReactComponent as Instagram } from './assets/instagram.svg';
+import { ReactComponent as Linkedin } from './assets/linkedin.svg';
+import { ReactComponent as Rocket } from './assets/rocket.svg';
+
+import challenge1 from '../challenge1/assets/desktop.png';
+import challenge2 from '../challenge2/assets/desktop.png';
+import challenge3 from '../challenge3/assets/capa.png';
+import challenge4 from '../challenge4/assets/capa.png';
+import challenge5 from '../challenge5/assets/capa.png';
 
 
 export function Home() {
@@ -25,11 +36,14 @@ export function Home() {
     <div className={
       darkMode 
       ? 
-      "bg-[url('src/pages/home/assets/Desktop-noturno.png')] bg-cover h-screen flex flex-col items-center py-14 px-[426px] transition-all"
+      "md:bg-[url('src/pages/home/assets/Desktop-noturno.png')] bg-[url('src/pages/home/assets/Mobile-noturno.png')] bg-cover flex flex-col gap-4 items-center pt-12 pb-7 md:px-[426px] px-5 transition-all"
       : 
-      "bg-[url('src/pages/home/assets/Desktop-diurno.png')] bg-cover h-screen flex flex-col items-center py-14 px-[426px] transition-all"
+      "md:bg-[url('src/pages/home/assets/Desktop-diurno.png')] bg-[url('src/pages/home/assets/Mobile-diurno.png')] bg-cover flex flex-col gap-4 items-center pt-12 pb-7 md:px-[426px] px-5 transition-all"
     }>
-      <div className='text-[#C6ADFF] text-5xl'>#boracodar</div>
+      <div className='flex justify-center items-center'>
+        <Rocket  height={50} />
+        <div className='text-[#C6ADFF] text-3xl'> #boracodar</div>
+      </div>
       <div>
         <button className='' onClick={toggleDarkMode}>
           {
@@ -42,10 +56,42 @@ export function Home() {
         </button>
       </div>
       <div>
-        <div className='flex'>
-          <img src={challenge1} className='w-[200px]' alt="" />
-          <span>Challenge 1</span>
-        </div>
+
+        <Carousel>
+
+          <div>
+            <img src={challenge1} alt="" />
+            <p className="legend">Challenge 1: Music Player</p>
+          </div>
+
+          <div>
+            <img src={challenge2} alt="" />
+            <p className="legend">Challenge 2: Product Card</p>
+          </div>
+          
+          <div>
+            <img src={challenge3} alt="" />
+            <p className="legend">Challenge 3: Buttons and Cursors</p>
+          </div>
+          
+          <div>
+            <img src={challenge4} alt="" />
+            <p className="legend">Challenge 4: Chat</p>
+          </div>
+          
+          <div>
+            <img src={challenge5} alt="" />
+            <p className="legend">Challenge 5: Calculator</p>
+          </div>
+
+        </Carousel>
+
+      </div>
+
+      <div className='flex gap-8 items-center'>
+        <Github className={darkMode ? 'fill-white cursor-pointer transition-all' : 'fill-black cursor-pointer transition-all'} />
+        <Instagram className={darkMode ? 'fill-white cursor-pointer transition-all' : 'fill-black cursor-pointer transition-all'} />
+        <Linkedin className={darkMode ? 'fill-white cursor-pointer transition-all' : 'fill-black cursor-pointer transition-all'} />
       </div>
     </div>
   );
