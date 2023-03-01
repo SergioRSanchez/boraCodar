10231ae47111df5097fb1f3eec6cc44e207873f3
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2023-02-28 23:09:53 -0300

    update challenge 8

--- src/pages/challenge8/index.jsx
+++ src/pages/challenge8/index.jsx
@@ -10,7 +10,9 @@ import { ReactComponent as DownPolygon } from './assets/downPolygon.svg'
 
 
 
-export function NetPromoterScore() {
+
+
+function NetPromoterScore() {
   return (
     <div className='bg-[#363447] flex flex-col gap-4 font-["Inter"] px-12 py-7 w-[296px] rounded-2xl shadow-[3px_4px_26px_0_rgba(0,0,0,0.25)]'>
       <h1 className='text-white text-2xl font-semibold text-center'>NPS geral</h1>
@@ -27,41 +29,10 @@ export function NetPromoterScore() {
 }
 
 
-export function ClosedSales() {
-  return (
-    <div className='bg-[#363447] flex flex-col items-center font-["Inter"] px-10 py-7 w-[351px] rounded-2xl shadow-[3px_4px_26px_0_rgba(0,0,0,0.25)]'>
-      <h1 className='text-white text-2xl font-semibold text-center mb-8'>Vendas fechadas</h1>
-      <div>
-        <svg className='w-[160px] h-[160px] relative'>
-          <circle r='70' cx='70' cy='70' className='w-[155px] h-[155px] fill-none stroke-[#464556] stroke-[10px] translate-x-[8px] translate-y-[8px]' style={{strokeDasharray: 440, strokeDashoffset: 0}}></circle>
-          <circle r='70' cx='70' cy='70' className='w-[155px] h-[155px] fill-none stroke-[#7568f0] stroke-[15px] translate-x-[8px] translate-y-[8px] transition-all' style={{strokeDasharray: 440, strokeDashoffset: 132}}></circle>
-        </svg>
-        <div className='relative -top-2 left-0 w-full h-full flex justify-center items-center text-white'><p className='relative bottom-24 left-10 text-lg font-bold'>70%</p><p className='relative bottom-20 right-5'>alcançada</p></div>
-      </div>
-      <div className='flex gap-5 '>
-        <div className='text-white text-sm flex gap-2'>
-          <div className='flex items-center gap-1 font-medium'>
-            <Expected />
-            <span>Esperado</span>
-          </div>
-          <span>100</span>
-        </div>
-
-        <div className='text-white text-sm flex gap-2'>
-          <div className='flex items-center gap-1 font-medium'>
-            <SalesEllipse />
-            <span>Alcançado</span>
-          </div>
-          <span>70</span>
-        </div>
-      </div>
-      
-    </div>
-  )
-}
 
 
-export function MonthlyGoal() {
+
+function MonthlyGoal() {
   const [ expect, setExpect ] = useState('')
   const [ reach, setReach ] = useState('')
   const [ percent, setPercent ] = useState(44)
@@ -111,7 +82,46 @@ export function MonthlyGoal() {
 }
 
 
-export function WeekSales() {
+function WeekSales() {
+  
+  const [ dom, setDom ] = useState(40)
+  const [ seg, setSeg ] = useState(116)
+  const [ ter, setTer ] = useState(76)
+  const [ qua, setQua ] = useState(160)
+  const [ qui, setQui ] = useState(130)
+  const [ sex, setSex ] = useState(120)
+  const [ sab, setSab ] = useState(68)
+  
+  const data = [
+    {day: 'domingo', value: dom},
+    {day: 'segunda-feira', value: seg},
+    {day: 'terça-feira', value: ter},
+    {day: 'quarta-feira', value: qua},
+    {day: 'quinta-feira', value: qui},
+    {day: 'sexta-feira', value: sex},
+    {day: 'sábado', value: sab},
+  ]
+
+  const biggestSales = data.reduce(function(a,b) {
+    return (a.value > b.value) ? a : b
+  })
+
+  const lowestSales = data.reduce(function(a,b) {
+    return (a.value < b.value) ? a : b
+  })
+
+  const [ showInput, setShowInput ] = useState(false)
+
+  function showInputFunction() {
+    if (showInput === false) {
+      setShowInput(true)
+    }
+    if (showInput === true) {
+      setShowInput(false)
+    }
+  }
+
+
   return (
     <div className='bg-[#363447] text-white flex flex-col gap-8 font-["Inter"] px-12 py-7 lg:w-screen lg:max-w-[1120px] rounded-2xl shadow-[3px_4px_26px_0_rgba(0,0,0,0.25)]'>
 
@@ -126,7 +136,9 @@ export function WeekSales() {
                 <UpPolygon />
                 <span className='text-sm font-medium'>Dia com mais vendas</span>
               </div>
-              <p className='font-medium text-2xl'>quarta-feira</p>
+              <p className='font-medium text-2xl'>
+                {biggestSales.day}
+              </p>
             </div>
 
             <div className='flex flex-col gap-8'>
@@ -134,50 +146,129 @@ export function WeekSales() {
                 <DownPolygon />
                 <span className='text-sm font-medium'>Dia com menos vendas</span>
               </div>
-              <p className='font-medium text-2xl'>domingo</p>
+              <p className='font-medium text-2xl'>
+                {lowestSales.day}
+              </p>
             </div>
 
           </div>
 
           <div className='sm:w-[500px] h-[200px] flex justify-between items-end'>
             <div className='flex flex-col items-center gap-1'>
-              <svg className='w-[10px] h-[39px] bg-[#32CCBC] rounded-full' />
-              <p className='text-xs'>dom</p>
+              <button onClick={showInputFunction}><svg className='w-[10px] bg-[#32CCBC] rounded-full' style={{height: `${dom}`}} /></button>
+              {
+                showInput
+                ?
+                <input onChange={(e) => setDom(e.target.value)} onBlur={showInputFunction} type="text" placeholder='40' className='text-xs w-[22px] text-white text-center bg-[#363447]'/>
+                :
+                <p className='text-xs'>dom</p>
+              }
             </div>
 
             <div className='flex flex-col items-center gap-1'>
-              <svg className='w-[10px] h-[115px] bg-[#32CCBC] rounded-full' />
-              <p className='text-xs'>seg</p>
+              <button onClick={showInputFunction}><svg className='w-[10px] bg-[#32CCBC] rounded-full' style={{height: `${seg}`}}/></button>
+              {
+                showInput
+                ?
+                <input onChange={(e) => setSeg(e.target.value)} onBlur={showInputFunction} type="text" placeholder='116' className='text-xs w-[22px] text-white text-center bg-[#363447]'/>
+                :
+                <p className='text-xs'>seg</p>
+              }
             </div>
             
             <div className='flex flex-col items-center gap-1'>
-              <svg className='w-[10px] h-[76px] bg-[#32CCBC] rounded-full' />
-              <p className='text-xs'>ter</p>
+              <button onClick={showInputFunction}><svg className='w-[10px] bg-[#32CCBC] rounded-full' style={{height: `${ter}`}}/></button>
+              {
+                showInput
+                ?
+                <input onChange={(e) => setTer(e.target.value)} onBlur={showInputFunction} type="text" placeholder='76' className='text-xs w-[22px] text-white text-center bg-[#363447]'/>
+                :
+                <p className='text-xs'>ter</p>
+              }
             </div>
             
             <div className='flex flex-col items-center gap-1'>
-              <svg className='w-[10px] h-[159px] bg-[#32CCBC] rounded-full' />
-              <p className='text-xs'>qua</p>
+              <button onClick={showInputFunction}><svg className='w-[10px] bg-[#32CCBC] rounded-full' style={{height: `${qua}`}}/></button>
+              {
+                showInput
+                ?
+                <input onChange={(e) => setQua(e.target.value)} onBlur={showInputFunction} type="text" placeholder='160' className='text-xs w-[22px] text-white text-center bg-[#363447]'/>
+                :
+                <p className='text-xs'>qua</p>
+              }
             </div>
             
             <div className='flex flex-col items-center gap-1'>
-              <svg className='w-[10px] h-[129px] bg-[#32CCBC] rounded-full' />
-              <p className='text-xs'>qui</p>
+              <button onClick={showInputFunction}><svg className='w-[10px] bg-[#32CCBC] rounded-full' style={{height: `${qui}`}}/></button>
+              {
+                showInput
+                ?
+                <input onChange={(e) => setQui(e.target.value)} onBlur={showInputFunction} type="text" placeholder='130' className='text-xs w-[22px] text-white text-center bg-[#363447]'/>
+                :
+                <p className='text-xs'>qui</p>
+              }
             </div>
             
             <div className='flex flex-col items-center gap-1'>
-              <svg className='w-[10px] h-[120px] bg-[#32CCBC] rounded-full' />
-              <p className='text-xs'>sex</p>
+              <button onClick={showInputFunction}><svg className='w-[10px] bg-[#32CCBC] rounded-full' style={{height: `${sex}`}}/></button>
+              {
+                showInput
+                ?
+                <input onChange={(e) => setSex(e.target.value)} onBlur={showInputFunction} type="text" placeholder='120' className='text-xs w-[22px] text-white text-center bg-[#363447]'/>
+                :
+                <p className='text-xs'>sex</p>
+              }
             </div>
             
             <div className='flex flex-col items-center gap-1'>
-              <svg className='w-[10px] h-[69px] bg-[#32CCBC] rounded-full' />
-              <p className='text-xs'>sab</p>
+              <button onClick={showInputFunction}><svg className='w-[10px] bg-[#32CCBC] rounded-full' style={{height: `${sab}`}}/></button>
+              {
+                showInput
+                ?
+                <input onChange={(e) => setSab(e.target.value)} onBlur={showInputFunction} type="text" placeholder='68' className='text-xs w-[22px] text-white text-center bg-[#363447]'/>
+                :
+                <p className='text-xs'>sab</p>
+              }
             </div>
           </div>
 
         </div>
         
+    </div>
+  )
+}
+
+function ClosedSales() {
+
+
+  return (
+    <div className='bg-[#363447] flex flex-col items-center font-["Inter"] px-10 py-7 w-[351px] rounded-2xl shadow-[3px_4px_26px_0_rgba(0,0,0,0.25)]'>
+      <h1 className='text-white text-2xl font-semibold text-center mb-8'>Vendas fechadas</h1>
+      <div>
+        <svg className='w-[160px] h-[160px] relative'>
+          <circle r='70' cx='70' cy='70' className='w-[155px] h-[155px] fill-none stroke-[#464556] stroke-[10px] translate-x-[8px] translate-y-[8px]' style={{strokeDasharray: 440, strokeDashoffset: 0}}></circle>
+          <circle r='70' cx='70' cy='70' className='w-[155px] h-[155px] fill-none stroke-[#7568f0] stroke-[15px] translate-x-[8px] translate-y-[8px] transition-all' style={{strokeDasharray: 440, strokeDashoffset: 132}}></circle>
+        </svg>
+        <div className='relative -top-2 left-0 w-full h-full flex justify-center items-center text-white'><p className='relative bottom-24 left-10 text-lg font-bold'>70%</p><p className='relative bottom-20 right-5'>alcançada</p></div>
+      </div>
+      <div className='flex gap-5 '>
+        <div className='text-white text-sm flex gap-2'>
+          <div className='flex items-center gap-1 font-medium'>
+            <Expected />
+            <span>Esperado</span>
+          </div>
+          <span>100</span>
+        </div>
+
+        <div className='text-white text-sm flex gap-2'>
+          <div className='flex items-center gap-1 font-medium'>
+            <SalesEllipse />
+            <span>Alcançado</span>
+          </div>
+          <span>70</span>
+        </div>
+      </div>
+      
     </div>
   )
 }
